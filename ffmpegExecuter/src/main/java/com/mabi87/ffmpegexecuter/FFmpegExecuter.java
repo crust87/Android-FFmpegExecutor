000ca6e37738bac677d52a167b142f81f1d1ceeb
mabi <crust87@gmail.com>
2015-05-22 21:51:06 +0900

    Create project

---
+++ ffmpegExecuter/src/main/java/com/mabi87/ffmpegexecuter/FFmpegExecuter.java
@@ -0,0 +1,145 @@
+/*
+ * HttpRequestBuilder
+ * https://github.com/mabi87/Android-FFmpegCommandExecuter
+ *
+ * Mabi
+ * crust87@gmail.com
+ * last modify 2015-05-22
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mabi87.ffmpegexecuter;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+public class FFmpegExecuter {
+    // Constants
+    private final static String FFMPEG_PATH = "ffmpeg";
+
+    // Components
+    private Context mContext;
+    private ArrayList<String> mCommands;
+    private OnReadProcessLineListener mOnReadProcessLineListener;
+
+    // Constructor
+    // copy ffmpeg to internal storage and change file permission
+    public FFmpegExecuter(Context context) {
+        mContext = context;
+        mCommands = new ArrayList<String>();
+
+        String[] libraryAssets = { "ffmpeg" };
+
+        File ffmpegPath = new File(mContext.getFilesDir().getAbsolutePath() + "/" + FFMPEG_PATH);
+        if(!ffmpegPath.exists()) {
+            ffmpegPath.mkdir();
+        }
+
+        for (int i = 0; i < libraryAssets.length; i++) {
+            try {
+                InputStream ffmpegInputStream = mContext.getAssets().open(libraryAssets[i]);
+                FileMover fm = new FileMover(ffmpegInputStream, ffmpegPath.getAbsolutePath() + "/" + libraryAssets[i]);
+                fm.moveIt();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Process process = null;
+
+        try {
+            String[] args = { "/system/bin/chmod", "755", ffmpegPath.getAbsolutePath() + "/ffmpeg" };
+            process = new ProcessBuilder(args).start();
+            try {
+                process.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            process.destroy();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Initialization
+    public void init() {
+        mCommands.clear();
+    }
+
+    public void putCommand(String command) {
+        mCommands.add(command);
+    }
+
+    public void executeCommand() throws IOException {
+        Process process = new ProcessBuilder(mCommands).redirectErrorStream(true).start();
+        ffmpegLog(process);
+    }
+
+    public void ffmpegLog(Process process) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null)  {
+            if(mOnReadProcessLineListener != null) {
+                mOnReadProcessLineListener.onReadProcessLine(line);
+            } else {
+                Log.v("FFmpegExecuter", line);
+            }
+        }
+    }
+
+    public void setOnReadProcessLineListener(OnReadProcessLineListener pOnReadProcessLineListener) {
+        mOnReadProcessLineListener = pOnReadProcessLineListener;
+    }
+
+    public interface OnReadProcessLineListener {
+        public abstract void onReadProcessLine(String line);
+    }
+
+    // Copy file
+    private class FileMover {
+
+        private InputStream mInputStream;
+        private String mDestination;
+
+        public FileMover(InputStream inputStream, String destination) {
+            mInputStream = inputStream;
+            mDestination = destination;
+        }
+
+        public void moveIt() throws IOException {
+
+            File destinationFile = new File(mDestination);
+            OutputStream destinationOut = new BufferedOutputStream(new FileOutputStream(destinationFile));
+
+            int numRead;
+            byte[] buf = new byte[1024];
+            while ((numRead = mInputStream.read(buf) ) >= 0) {
+                destinationOut.write(buf, 0, numRead);
+            }
+
+            destinationOut.flush();
+            destinationOut.close();
+        }
+    }
+}
